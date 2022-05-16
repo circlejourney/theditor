695843d081893598763352d4842d942f4cff6def
circlejourney <sceptile_rayquaza@hotmail.com>
2022-05-16 12:56:47 +1000

    Sync with Toyhouse source code updates
    Updated the source files to match Toyhouse's recent update, and made another (hopefully final) effort at improving the cache busting. Removed unstable version and now managing watermark feature via Github.

--- build_20220509/index.php
+++ build_20220509/index.php
@@ -1,7 +1,11 @@
+<?php
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php $lastUpdate = "20210625"; ?>
+        <?php $lastUpdate = "20220516"; ?>
         
         <script>
             const lastUpdate = <?php echo $lastUpdate ?>;
@@ -28,17 +32,22 @@
     	<!-- Font Awesome -->
         <script src="https://kit.fontawesome.com/0ddae54ad8.js" crossorigin="anonymous"></script>
         
-        <script src="script.js?<?php echo $lastUpdate ?>" type="text/javascript"></script>
-        <link rel="stylesheet" href="style.css?<?php echo $lastUpdate ?>">
+        <script src="/build_<?php echo $lastUpdate ?>/script.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="/build_<?php echo $lastUpdate ?>/style.css">
             
     </head>
     <body>
     
-    <div id="loader">
-        <img style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" src="https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif">
+    <div id="loader" style="text-align: center; display: flex; flex-direction: column; justify-content: center;">
+        <div class="loader-inner">
+            <img src="https://media0.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" style="margin: -50px 0;">
+            <p style="font-size: 20pt;">Stuck on the loading screen? <a href="https://toyhou.se/~messages/create/circlejourney">Send me a bug report</a>.</p>
+            <p style="font-size: 20pt;">If you suspect your code has crashed the editor, <a href="#">click here</a> to download your code as text files and hard reset the editor.</p>
+            <p>Fun fact! You can also hard reset by typing <code>Please reset!</code> in the HTML panel.</p>
+        </div>
     </div>
-    
     <div id="info" class="d-none">
+        <div id="info-back" onclick="showInfo();"></div>
         <div class="card">
             <div class="card-header d-flex justify-content-between">
                 <h2 class="m-0 w-75 d-inline-block p-2">Welcome to Circlejourney's Code Editor!</h2>
@@ -48,7 +57,7 @@
             <div id="info-main" class="card-block text-center">
                 <div id="notes"></div>
                 
-                <a class="btn btn-primary" onclick="showInfo()">Got it!</a>
+                <a class="btn btn-primary text-white" onclick="showInfo()">Got it!</a>
                 <a class="btn btn-secondary" data-toggle="collapse" data-target="#changelog">Changelog</a>
                 <a class="btn btn-secondary" data-toggle="collapse" data-target="#issues">Known issues</a>
                 <a class="btn btn-secondary" data-toggle="collapse" data-target="#versions">Versions</a>
@@ -80,7 +89,7 @@
     </div>
     
     <div id="main">
-        <iframe src="frame.html?<?php echo $lastUpdate ?>" id="frame" class="d-flex">
+        <iframe src="/build_<?php echo $lastUpdate ?>/frame.html" id="frame" class="d-flex">
         </iframe>
         
         <div id="adjustbar" class="progress-bar progress-bar-striped bg-secondary">
@@ -96,11 +105,8 @@
                     HTML
                     <span class="panel-options" id="html-options">
                         <a class="edit-button" onclick="insertLorem('html')" data-toggle="tooltip" title="Insert lorem ipsum"><i class="fas fa-text"></i></a>
-                        
-                        <a class="edit-button" onclick="uploadFileDialogue('html')" data-toggle="tooltip" title="Upload file"><i class="fa fa-file-upload"></i></a>
-                        
+                        <a class="edit-button" onclick="uploadFileDialogue('html')" data-toggle="tooltip" title="Upload file"><i class="fa fa-file-upload"></i></a>                        
                         <a class="edit-button" onclick="downloadFile('html')" data-toggle="tooltip" title="Save as file"><i class="fa fa-save"></i></a>
-                        
                         <a class="edit-button clear-button" id="clear-html" onclick="clearEditor(this, 'html')" data-toggle="tooltip" title="Clear"><i class="fa fa-trash"></i></a>
                     </span>
                 </div>
@@ -126,37 +132,15 @@
             </div>
             
             <div id="fields">
-                <div id="html-editor" class="html-visible"></div>
-                <div class="css-visible" id="css-editor"></div>
-                <div class="text-visible" id="text-editor"></div>
+                <div class="html-visible editor-panel" id="html-editor"></div>
+                <div class="css-visible editor-panel" id="css-editor"></div>
+                <div class="text-visible editor-panel" id="text-editor"></div>
             </div>
         </div>
             
         <div id="footer" class="bg-light text-dark d-flex justify-content-between ">
             <div id="footer-left">
                 <a class="btn btn-secondary" onclick="showInfo()"><i class="fa fa-info"></i></a>
-    
-                <div id="import" class="dropdown d-sm-inline">
-                  <a class="btn btn-secondary dropdown-toggle" id="dropdownbutton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Import profile
-                  </a>
-                
-                  <div class="dropdown-menu px-2" aria-labelledby="dropdownbutton2">
-                      <div class="d-flex flex-column">
-                          <div class="d-flex mb-1">
-                            <input type="text" id="char-id" class="form-control mr-1 import-button" placeholder="Enter ID"></input>
-                             <abbr class="d-inline-block" title="The numerical ID (for a character profile, e.g. 1776660) or username (for a user profile). Page must be public and not have a warning." data-toggle="tooltip">
-                                 <i class="fa fa-question-circle"></i>
-                             </abbr>
-                         </div>
-                         <div class="d-flex">
-                        <button class="btn btn-primary mr-1 import-button" id="import-meta" onclick="startImport('meta')">Import meta</button>
-                        <button class="btn btn-primary mr-1 import-button" id="import-html" onclick="startImport('code')">Import HTML</button>
-                        <button class="btn btn-primary" onclick="importedmeta=null; importedcode=null; localStorage.removeItem('th_cj_importedmeta'); localStorage.removeItem('th_cj_importedcode'); switchTo(activemode);">Reset</button>
-                        </div>
-                    </div>
-                  </div>
-                </div>
     
                 <div id="themes" class="dropdown d-sm-inline">
                   <a class="btn btn-secondary dropdown-toggle" id="dropdownbutton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -183,6 +167,11 @@
                     <a class="dropdown-item" onclick="switchTo('char-profile')">
                         Character profile
                     </a>
+                    
+                    <a class="dropdown-item" onclick="switchTo('char-tab')">
+                        Character basic tab
+                    </a>
+                    
                     <a class="dropdown-item" onclick="switchTo('thread')">
                         Forum thread
                     </a>
@@ -198,28 +187,70 @@
                     <a class="dropdown-item" onclick="switchTo('warning')">
                         Warning
                     </a>
+                    
+                    <a class="dropdown-item" onclick="switchTo('char-gallery')">
+                        Character gallery (static)
+                    </a>
+                    
+                    <a class="dropdown-item" onclick="switchTo('char-library')">
+                        Character library (static)
+                    </a>
                     </div>
-                </div> 
+                </div>
+                
+    
+                <div id="import" class="dropdown d-sm-inline">
+                  <a class="btn btn-secondary dropdown-toggle" id="dropdownbutton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Import profile
+                  </a>
+                
+                  <div class="dropdown-menu px-2" aria-labelledby="dropdownbutton2">
+                      <div class="d-flex flex-column">
+                          <div class="d-flex mb-1">
+                            <input type="text" id="char-id" class="form-control mr-1 import-button" placeholder="Enter ID"></input>
+                             <abbr class="d-inline-block" title="The numerical ID (for a character profile, e.g. 1776660) or username (for a user profile). Page must be public and not have a warning." data-toggle="tooltip">
+                                 <i class="fa fa-question-circle"></i>
+                             </abbr>
+                         </div>
+                         <div class="d-flex">
+                        <button class="btn btn-primary mr-1 import-button" id="import-meta" onclick="startImport('meta')">Import meta</button>
+                        <button class="btn btn-primary mr-1 import-button" id="import-html" onclick="startImport('code')">Import HTML</button>
+                        <button class="btn btn-primary" onclick="importedmeta=null; importedcode=null; localStorage.removeItem('th_cj_importedmeta'); localStorage.removeItem('th_cj_importedcode'); switchTo(activemode);">Reset</button>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                
+                <a id="toggle-sidebar" class="btn btn-secondary" onclick="frame.contentWindow.toggleUI()">
+                     <i id='sidebar-toggle-icon' class="fa fa-chevron-left"></i> Sidebar
+                </a>
+                        
                 <a class="hide-small btn btn-primary" href="/watermark" target="_blank">Watermark previewer</a>
             </div>
             
             <div id="footer-right">
-                <span>
-                    <input type="checkbox" class="hide-small" id="vertical" onchange="setVerticalLayout();"> <label for="vertical" class="hide-small">Vertical layout</label>
+                <span class="checkbox-container">
+                    <input type="checkbox" class="hide-small" id="vertical" onchange="setVerticalLayout();">&nbsp;<label for="vertical" class="hide-small">Vertical</label>
                 </span>
                 
-                <span>
-                    <input type="checkbox" id="auto" onchange="setAutoUpdate();" checked="true"> <label for="auto">Auto-update</label>
+                <span class="checkbox-container">
+                    <input type="checkbox" id="auto" onchange="setAutoUpdate();" checked="true">&nbsp;<label for="auto">Auto-update</label>
                 </span>
                 
-                <span>
-                    <input type="checkbox" id="css-panel" onclick="setCSSPanel();" checked="true"> <label for="css-panel">CSS</label>
+                <span class="checkbox-container">
+                    <input type="checkbox" id="html-panel" onclick="setHTMLPanel();" checked="true">&nbsp;<label for="html-panel">HTML</label>
                 </span>
                 
-                <span class="show-small"><br></span>
+                <span class="checkbox-container">
+                    <input type="checkbox" id="css-panel" onclick="setCSSPanel();" checked="true">&nbsp;<label for="css-panel">CSS</label>
+                </span>
                 
-                <span>
-                    <input type="checkbox" id="text-panel" onclick="setTextPanel();" checked="true"> <label for="text-panel">Scratch pad</label>
+                <span class="checkbox-container">
+                    <input type="checkbox" id="text-panel" onclick="setTextPanel();" checked="true">&nbsp;<label for="text-panel">Scratchpad</label>
+                </span>
+                
+                <span class="checkbox-container">
+                    <input type="checkbox" id="big-text" onclick="toggleBigFont();"> <label for="big-text">Big text</label>
                 </span>
                 <a class="btn btn-primary update-btn d-inline-block" id="update-preview" onclick="updateCode()">Update preview</a>
             </div>
--- watermark/script.js
+++ watermark/script.js
@@ -1,114 +1,1 @@
-var mycanvas, mycontext, baseimage, watermark, baseimage, watermark;
-var position = "t";
-
-window.onload = function() {
-    mycanvas = document.getElementById('canvas'),
-    mycontext = canvas.getContext('2d');
-    baseimage = new Image();
-    baseimage.src="vanitasbg.jpg";
-    watermark = new Image();
-    watermark.src="default.png";
-
-    watermark.onload = baseimage.onload = function() {
-        draw();
-    }
-    
-    $("#baseinput").change(updateBase);
-    $("#watermarkinput").change(updateWatermark);
-    $("#position").change(updatePosition)
-}
-
-function updateBase(event) {
-    var file = event.target.files[0]; // File
-	if(!file) return false;
-	
-	if(file.type.match('image.*')) {
-	    var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = function (e) {
-    	    baseimage.src=e.target.result;
-    	    baseimage.onload=function() {
-                draw();
-    	    }
-        }
-
-    } else alert("Please upload an image!");
-}
-
-function updateWatermark(event) {
-    var file = event.target.files[0]; // File
-	
-	if(!file) return false;
-	
-	if(file.type.match('image.*')) {
-	    var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = function (e) {
-    	    watermark.src=e.target.result;
-        }
-
-    } else alert("Please upload an image!");
-}
-
-function updatePosition(event) {
-    position = $(event.target).find("option:selected").val();
-    draw();
-}
-
-function draw() {
-    
-    mycontext.clearRect(0, 0, mycanvas.width, mycanvas.height);
-    mycanvas.width=baseimage.width;
-    mycanvas.height=baseimage.height;
-    
-    mycontext.drawImage(baseimage, 0, 0);
-    
-    if(position=="t") {
-        
-        for(var i=0; i < Math.ceil(mycanvas.width/watermark.width); i++) {
-            for(var j=0; j<Math.ceil(mycanvas.height/watermark.height); j++) {
-                mycontext.drawImage(watermark, i*watermark.width, j*watermark.height);
-            }
-        }
-        
-    } else if(position=="s") {
-        
-        var wRatio = mycanvas.width/watermark.width;
-        var hRatio = mycanvas.height/watermark.height;
-        var ratio;
-        wRatio < hRatio ? ratio = wRatio : ratio = hRatio;
-        var newW = watermark.width * ratio;
-        var newH = watermark.height * ratio;
-        mycontext.drawImage(watermark, canvas.width/2-newW/2, canvas.height/2-newH/2, newW, newH);
-        
-    } else {
-        
-        var left=0;
-        var top=0;
-        
-        switch(position) {
-            case "c":
-                left = mycanvas.width/2-watermark.width/2;
-                top = mycanvas.height/2-watermark.height/2;
-                break;
-                
-            case "tr":
-                left = mycanvas.width-watermark.width;
-                break;
-                
-            case "bl":
-                top = mycanvas.height-watermark.height
-                break;
-                
-            case "br":
-                left = mycanvas.width-watermark.width
-                top = mycanvas.height-watermark.height;
-                break;
-            
-            default: 
-                left = 0;
-                top = 0;
-        }
-        mycontext.drawImage(watermark, left, top);
-    }
-}
+var mycanvas, mycontext, baseimage, watermark, baseimage, watermark;var position = "t";var imgurlpattern = /https*\:\/\/.*.(png|jpg|gif)/;window.onload = function() {    mycanvas = document.getElementById('canvas'),    mycontext = canvas.getContext('2d');    baseimage = new Image();    baseimage.src="vanitasbg.jpg";    $(baseimage).on("load", function(){        draw();    });    watermark = new Image();    watermark.src="default.png";    $(watermark).on("load", function(){        draw();    });    watermark.onload = baseimage.onload = function() {        draw();    }    $("#baseinput").change(updateBase);    $("#watermarkinput").change(updateWatermark);    $("#baseurl").change(updateBase);    $("#watermarkurl").change(updateWatermark);    $("#position").change(updatePosition);}function updateBase(event) {    if(event.target.files) {        var file = event.target.files[0]; // File    	if(!file) return false;    	if(file.type.match('image.*')) {    	    var reader = new FileReader();            reader.readAsDataURL(file);            reader.onloadend = function (e) {        	    baseimage.src=e.target.result;            }        } else alert("Please upload an image file!");    } else {        if(!imgurlpattern.test(event.target.value)) {            alert("Please enter a link to an image file!");            return false;         }        baseimage.src=event.target.value;    }    event.target.value=null;}function updateWatermark(event) {    if(event.target.files) {        var file = event.target.files[0]; // File    	if(!file) return false;    	if(file.type.match('image.*')) {    	    var reader = new FileReader();            reader.readAsDataURL(file);            reader.onloadend = function (e) {        	    watermark.src=e.target.result;            }        } else alert("Please upload an image file!");    } else {        if(!imgurlpattern.test(event.target.value)) {            alert("Please enter a link to an image file!");            return false;         }        watermark.src=event.target.value;    }    event.target.value=null;}function updatePosition(event) {    position = $(event.target).find("option:selected").val();    draw();}function draw() {    mycontext.clearRect(0, 0, mycanvas.width, mycanvas.height);    mycanvas.width=baseimage.width;    mycanvas.height=baseimage.height;    mycontext.drawImage(baseimage, 0, 0);    if(position=="t") {                for(var i=0; i < Math.ceil(mycanvas.width/watermark.width); i++) {            for(var j=0; j<Math.ceil(mycanvas.height/watermark.height); j++) {                mycontext.drawImage(watermark, i*watermark.width, j*watermark.height);            }        }            } else if(position=="s") {                var wRatio = mycanvas.width/watermark.width;        var hRatio = mycanvas.height/watermark.height;        var ratio = Math.min(mycanvas.width/watermark.width, mycanvas.height/watermark.height);        var newW = watermark.width * ratio;        var newH = watermark.height * ratio;        mycontext.drawImage(watermark, canvas.width/2-newW/2, canvas.height/2-newH/2, newW, newH);            } else {                let left, top;        switch(position) {            case "c":                left = mycanvas.width/2-watermark.width/2;                top = mycanvas.height/2-watermark.height/2;                break;            case "tr":                left = mycanvas.width-watermark.width;                break;            case "bl":                top = mycanvas.height-watermark.height                break;            case "br":                left = mycanvas.width-watermark.width                top = mycanvas.height-watermark.height;                break;                            default:                 left = 0;                top = 0;        }        mycontext.drawImage(watermark, left, top);            }}
