0d3cd32b7c70766a9454b27568de228ef5009542
Amari Low <sceptile_rayquaza@hotmail.com>
2023-03-27 19:50:56 +1000

    Fix red/green/blue turning into selector bug

--- src/ace-colorpicker.js
+++ src/ace-colorpicker.js
@@ -441,7 +441,6 @@ function matches(str) {
             }
         }
     }
-
     var pos = { next: 0 };
     result.forEach(function (item) {
         var startIndex = str.indexOf(item.color, pos.next);
@@ -9467,10 +9466,10 @@ var ColorView = function () {
 
                     // FIXME: Exception handling when the highlight does not turn into color due to the scss function name
                     // LINK : https://github.com/ajaxorg/ace/blob/cbcb78c3a7c5e642d615a9f5665a44dbb94d3e92/lib/ace/mode/scss_highlight_rules.js#L43-L48
-                    rules[stateName].unshift({
+                    /* rules[stateName].unshift({
                         token: "color",
                         regex: "blue|green|red"
-                    });
+                    }); */
                 }
             }
             // force recreation of tokenizer
