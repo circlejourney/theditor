db75c4c6cef0fe650edb104b812efcd063a7410b
Amari Low <sceptile_rayquaza@hotmail.com>
2023-03-27 16:56:48 +1000

    Adds colorpicker dependencies to root index.php

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
         <!-- Misc libraries -->
     	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="/src/ace-builds/src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+        <link rel="stylesheet" href="/src/ace-colorpicker.css" />
+        <script type="text/javascript" src="/src/ace-colorpicker.js" ></script>
         <script src="../src/sass.js-master/dist/sass.js">
         </script>
         
